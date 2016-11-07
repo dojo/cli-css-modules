32d701766275edd41957b07e8e009edd7d6dca74
bitpshr <paul@paulbouchon.net>
2016-11-07 13:13:32 -0500

    Tests

--- tests/unit/main.ts
+++ tests/unit/main.ts
@@ -28,73 +28,25 @@ describe('main', () => {
 		main.default.register(helper);
 		assert.deepEqual(
 			helper.yargs.option.firstCall.args,
-			[ 'c', {
-				alias: 'cssOut',
-				describe: 'directory to write CSS modules',
-				demand: false,
-				type: 'string'
-			} ]
-		);
-		assert.deepEqual(
-			helper.yargs.option.secondCall.args,
-			[ 't', {
-				alias: 'tsOut',
-				describe: 'directory to write TS modules',
-				demand: false,
+			[ 'o', {
+				alias: 'out',
+				describe: 'directory to write CSS module declaration files',
+				demand: true,
 				type: 'string'
 			} ]
 		);
 	});
 
-	it('should create a CSS module file', () => {
+	it('should create a CSS module declaration file', () => {
 		const writeFileStub: sinon.SinonStub = sandbox.stub(fs, 'writeFile', function (name: string, content: string, done: Function) {
 			done();
 		});
 		return main.default.run({}, {
-			cssOut: '.',
+			out: '.',
 			_: [null, null, 'tests/support/test.css']
 		}).then(() => {
 			assert.isTrue(writeFileStub.calledOnce);
-			assert.strictEqual(writeFileStub.getCall(0).args[0], 'test.css');
+			assert.strictEqual(writeFileStub.getCall(0).args[0], 'test.d.ts');
 		});
-	});
-
-	it('should create a TS module file', () => {
-		const writeFileSyncStub: sinon.SinonStub = sandbox.stub(fs, 'writeFileSync');
-		return main.default.run({}, {
-			tsOut: '.',
-			_: [null, null, 'tests/support/test.css']
-		}).then(() => {
-			assert.isTrue(writeFileSyncStub.calledOnce);
-			assert.strictEqual(writeFileSyncStub.getCall(0).args[0], './test.ts');
-		});
-	});
-
-	it('should not create a CSS module file if cssOut not specified', () => {
-		const writeFileStub: sinon.SinonStub = sandbox.stub(fs, 'writeFile', function (name: string, content: string, done: Function) {
-			done();
-		});
-		return main.default.run({}, {
-			_: [null, null, 'tests/support/test.css']
-		}).then(() => {
-			assert.isFalse(writeFileStub.called);
-		});
-	});
-
-	it('should not create a TS module file if tsOut not specified', () => {
-		const writeFileSyncStub: sinon.SinonStub = sandbox.stub(fs, 'writeFileSync');
-		return main.default.run({}, {
-			_: [null, null, 'tests/support/test.css']
-		}).then(() => {
-			assert.isFalse(writeFileSyncStub.called);
-		});
-	});
-
-	it ('should reject if CSS file is invalid', function (this: any) {
-		const dfd: any = this.async();
-
-		main.default.run({}, {
-			_: [null, null, 'tests/support/invalid.css']
-		}).then(() => { }, dfd.callback(() => {}));
 	});
 });
