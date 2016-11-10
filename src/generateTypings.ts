e85aee68b12909fd5f453f73e68f454f62529e35
bitpshr <paul@paulbouchon.net>
2016-11-10 13:07:51 -0500

    PR feedback
    * use async functions instead of lib
    * streamline command file
    * capitalize output
    * demand an input parameter

---
+++ src/generateTypings.ts
@@ -0,0 +1,76 @@
+import { Argv } from 'yargs';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as chalk from 'chalk';
+import * as mkdirp from 'mkdirp';
+const DtsCreator: any = require('typed-css-modules');
+const read: any = require('read-file-stdin');
+import * as globby from 'globby';
+
+export interface TypingsArgs extends Argv {
+	in: string;
+	out: string;
+}
+
+function readFile(file: string) {
+	return new Promise((resolve, reject) => {
+		read(file, function (err: any, buffer: Buffer) {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			resolve(buffer);
+		});
+	});
+}
+
+function execute(declarations: {}, creator: any) {
+	return new Promise((resolve, reject) => {
+		creator.create('', declarations).then(function (result: any) {
+			resolve(result);
+		}).catch(reject);
+	});
+}
+
+function write(name: string, content: any) {
+	return new Promise((resolve, reject) => {
+		mkdirp(path.dirname(name), function(err: any) {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			fs.writeFile(name, content.formatted, function (err) {
+				if (err) {
+					reject(err);
+					return;
+				}
+				console.log(chalk.green(name));
+				resolve();
+			});
+		});
+	});
+};
+
+async function processFile(args: TypingsArgs, creator: any, file: string) {
+	const outputPath: string = path.join(args.out, path.basename(file) + '.d.ts');
+
+	let contents = await readFile(file);
+	let typings = await execute(contents, creator);
+	return await write(outputPath, typings);
+}
+
+export function generate(args: TypingsArgs): Promise<any> {
+	let creator: any = new DtsCreator();
+	console.log(args.in, args);
+	const inputFiles: string[] = globby.sync(args.in);
+
+	const operations: Promise<any>[] = inputFiles.map((file) => {
+		return processFile(args, creator, file);
+	});
+
+	return Promise.all(operations).then(() => {
+		console.log('\nDone!');
+	});
+}
--- src/main.ts
+++ src/main.ts
@@ -1,82 +1,20 @@
 import { Command, Helper } from 'dojo-cli/interfaces';
-import { Argv } from 'yargs';
-import * as fs from 'fs';
-import * as path from 'path';
-const DtsCreator: any = require('typed-css-modules');
-const read: any = require('read-file-stdin');
-const globby: any = require('globby');
-const async: any = require('async');
-const mkdirp: any = require('mkdirp');
-const chalk: any = require('chalk');
-
-interface TypingsArgs extends Argv {
-	out: string;
-}
-
-function processFile(args: TypingsArgs, creator: any, file: string, done: Function): void {
-	const outputPath: string = path.join(args.out, path.basename(file) + '.d.ts');
-
-	function execute(declarations: string, done: Function): void {
-		creator.create('', declarations).then(function (result: any) {
-			done(null, result);
-		}).catch(done);
-	};
-
-	function write(name: string, content: any, done: Function): void {
-		async.parallel([
-			async.apply(function(name: string, content: string, done: Function) {
-				mkdirp(path.dirname(name), function(err: any) {
-					if (err) {
-						done(err);
-					}
-					else if (args.out) {
-						fs.writeFile(name, content, done);
-						console.log(chalk.green(name));
-					} else {
-						done();
-					}
-				});
-			}, name, content.formatted)
-		], done);
-	};
-
-	async.waterfall([
-		async.apply(read, file),
-		execute,
-		async.apply(write, outputPath)
-	], done);
-}
-
-function generate(args: TypingsArgs): Promise<any> {
-	let creator: any = new DtsCreator();
-	const inputFiles: string[] = globby.sync(args._.splice(2));
-
-	return new Promise((resolve, reject) => {
-		async.each(inputFiles, processFile.bind(null, args, creator), function(err: any) {
-			if (err) {
-				reject(err);
-				return;
-			}
-			console.log('\nDone!');
-			resolve({});
-		});
-	});
-}
+import { TypingsArgs, generate } from './generateTypings';
 
 const command: Command = {
-	description: 'generate CSS module type declaration files',
+	description: 'Generate CSS module type declaration files',
 	register(helper: Helper) {
-		helper.yargs.option('o', {
-			alias: 'out',
-			describe: 'directory to write CSS module declaration files',
+		helper.yargs.option('i', {
+			alias: 'in',
+			describe: 'Input CSS module file(s)',
 			demand: true,
 			type: 'string'
 		});
-		helper.yargs.check(function(argv: any) {
-			if (argv._.length === 2) {
-				throw 'input CSS file(s) must be specified';
-			}
-			return true;
+		helper.yargs.option('o', {
+			alias: 'out',
+			describe: 'Directory to write CSS module declaration files',
+			demand: true,
+			type: 'string'
 		});
 
 		return helper.yargs;
