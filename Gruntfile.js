9c55b4e0192acdc48ecbe94e2dd9f44526c22109
Anthony Gubler <agubler@sitepen.com>
2016-11-10 19:57:14 +0000

    remove copy package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,7 @@
 module.exports = function (grunt) {
-	require('grunt-dojo2').initConfig(grunt, {
-		copy: {
-			staticDistFiles: {
-				files: [
-					{ src: 'package.json', dest: 'dist/umd/package.json' }
-				]
-			},
-			staticDevFiles: {
-				files: [
-					{ src: 'package.json', dest: '_build/package.json' }
-				]
-			}
-		}
-	});
+	require('grunt-dojo2').initConfig(grunt, { });
 
 	grunt.registerTask('ci', [
 		'intern:node'
 	]);
-
-	grunt.registerTask('dist', grunt.config.get('distTasks').concat(['copy:staticDistFiles']));
-	grunt.registerTask('dev', grunt.config.get('devTasks').concat(['copy:staticDevFiles']));
 };
