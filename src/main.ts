d587554be8d45c8eb43b357ed0c7be9affe382ca
James Donaghue <james.donaghue@gmail.com>
2016-12-05 14:01:38 -0500

    Fix help so it displays per grouping

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 import { Command, Helper } from 'dojo-cli/interfaces';
 import { TypingsArgs, generate } from './generateTypings';
+import { Options } from 'yargs';
 
 const command: Command = {
 	description: 'Generate CSS module type declaration files',
-	register(helper: Helper) {
-		helper.yargs.option('i', {
+	register(options: (key: string, options: Options) => void): void {
+		options('i', {
 			alias: 'in',
 			describe: 'Input CSS module file(s)',
 			demand: true,
 			type: 'string'
 		});
-		helper.yargs.option('o', {
+		options('o', {
 			alias: 'out',
 			describe: 'Directory to write CSS module declaration files',
 			demand: true,
 			type: 'string'
 		});
-
-		return helper.yargs;
 	},
 	run(helper: Helper, args: TypingsArgs) {
 		console.log('\nGenerating CSS module typings...\n');
